7ef13a41c423374dd64620792cec66488161deab
FadiAlashqar <fadialshqar.fa01@gmail.com>
2025-03-16 23:18:09 +0100

    [UPD] es 2.3 done

--- 03-strings-bonus/script.js
+++ 03-strings-bonus/script.js
@@ -16,7 +16,7 @@ console.log(citySubstring.slice(4, 8)); // Output: "York"
 
 // 3. Sostituzione
 // Sostituisci 'York' con 'Delhi'
-const cityReplaced = '';
+const cityReplaced = city.replace("York", "Delhi");
 console.log(cityReplaced); // Output:  "New Delhi"
 
 // 4. Template literal
